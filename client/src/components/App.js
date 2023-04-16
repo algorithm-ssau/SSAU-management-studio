a79d13633664b59bf542a4d410d7bfe7f85c6362
TopyrkinaAlina <toperkinaaa@gmail.com>
2023-04-16 23:01:00 +0400

    AlinaTopyrkina: navbar layout

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+//import './App.css';
+import Navbar from './navbar/Navbar';
+import './app.css'
+
+function App() {
+  return (
+    <div className='app'>
+      <Navbar/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import './navbar.css'
+import Logo from '..//..//assets/img/navbar-logo.svg'
+
+const Navbar = () => {
+    return (
+        <div className = "navbar">
+            <div className="container">
+            <img src = {Logo} alt ="" className="navbar__logo"> </img>
+            <div className="navbar__header">SMART BOARD</div>
+            <div className="navbar_login">Войти</div>
+            <div className="navbar_registration">Регистрация</div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
