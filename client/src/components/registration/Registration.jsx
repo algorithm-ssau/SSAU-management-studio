3bb808f1b3ce7c84581c4fdb21e163a4f645b890
TopyrkinaAlina <toperkinaaa@gmail.com>
2023-04-17 22:34:33 +0400

    AlinaTopyrkina: Asynchronous requests to the server

--- client/src/components/registration/Registration.jsx
+++ client/src/components/registration/Registration.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import './registration.css';
 import Input from "../utils/input/Input";
+import { registration } from '../../actions/user';
 
 const Registration = () => {
     const [email, setEmail] = useState ("")
@@ -10,7 +11,7 @@ const Registration = () => {
             <div className="registration__header">Регистрация</div>
             <Input value={email} setValue={setEmail} type="text" placeholder="Введите email..."/>
             <Input value={password} setValue={setPassword} type="password" placeholder="Введите пароль..."/>
-            <button className="registration__btn">Войти</button>
+            <button className="registration__btn" onClick={() => registration(email,password)}>Войти</button>
         </div>
     );
 };
