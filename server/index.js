c80a44886edeb97da7d7e094895ec2850d78fbfe
MininVyacheslav <01noname@mail.ru>
2023-05-01 14:20:47 +0400

    Обновление

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const config = require("config")
+const authRouter = require("./auth.routes")
+const fileRouter = require("./file.routes")
+const app = express()
+const PORT = config.get('serverPort')
+const corsMiddleware = require('./middleware/cors.middleware')
+
+app.use(corsMiddleware)
+app.use(express.json())
+app.use("/api/auth", authRouter)
+app.use("/api/files", fileRouter)
+
+const start = async () => {
+    try{
+        mongoose.connect(config.get("dbURL"))
+
+
+        app.listen(PORT, () => {
+            console.log('Server started on port', PORT)
+        })
+
+    } catch(e){
+
+    }
+}
+
+start()
---
+++ server/services/file.service.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+const File = require('../models/File')
+const config = require('config')
+const { resolve } = require('path')
+const { rejects } = require('assert')
+
+class FileService{
+
+    createDir(file) {
+        const fileParh = '${config.get(file.path)}\\${file.user}\\${file.path}'
+        return new Promise (((resolve, reject) => {
+            try {
+                if (!fs.existsSync(file)) {
+                    fs.mkdirSync(filePath)
+                    return resolve({message: 'File was created'})
+                } else {
+                    return reject({message: 'File already exist'})
+                }
+
+            } catch (e) {
+                return reject( {message: 'File error'})
+            }
+        }))
+
+    }
+
+}
+
+module.exports = new FileService()
