fc4910933eb487dfecf235be09532b2d7fb1ea69
Danielips <dmachukhin56@gmail.com>
2023-05-16 10:37:17 +0400

    Всплывающее окно PopUp, возможность создания папок, исправление ошибок связи клиента и сервера

--- client/src/actions/user.js
+++ client/src/actions/user.js
@@ -35,7 +35,7 @@ export const auth =  () => {
     return async dispatch =>{
         try {
             const response = await axios.get(`http://localhost:5000/api/auth/auth`,
-                {headers: {Authorization: "Bearer ${localStorage.getItem('token')}"}}
+                {headers: {Authorization: `Bearer ${localStorage.getItem('token')}`}}
                 )
             dispatch(setUser(response.data.user))
             localStorage.setItem('token', response.data.token)
--- client/src/components/disk/disk.jsx
+++ client/src/components/disk/disk.jsx
@@ -1,8 +1,10 @@
 import React, {useEffect} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {getFiles} from "../../actions/file";
+import {createDir, getFiles} from "../../actions/file";
 import FileList from "./fileList/fileList";
 import './disk.css'
+import Popup from './Popup';
+import { setPopupDisplay } from '../../reducers/fileReducer';
 
 const Disk = () => {
     const dispatch = useDispatch()
@@ -12,14 +14,19 @@ const Disk = () => {
         dispatch(getFiles(currentDir))
     }, [currentDir])
 
+    function showPopupHandler(){
+        dispatch(setPopupDisplay('flex'))
+    }
+
     return (
         <div className="disk">
             <div className="disk_btns">
                 <button className="disk_back">Назад</button>
-                <button className="disk_create">Создать папку</button>
+                <button className="disk_create" onClick={() => showPopupHandler()}>Создать папку</button>
 
             </div>
             <FileList/>
+            <Popup/>
         </div>
     );
 };
