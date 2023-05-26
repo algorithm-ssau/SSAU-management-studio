99c505504623508d92ec8f1545584b645a06eb23
MininVyacheslav <01noname@mail.ru>
2023-05-26 10:58:16 +0400

    Удаление(не доделано)

--- server/controllers/fileConroller.js
+++ server/controllers/fileConroller.js
@@ -61,14 +61,20 @@ class FileController {
                 file.mv(path)
 
                 const type = file.name.split('.').pop()
+                /*let filePath = file.name
+                if(parent)
+                {
+                    filePath = parent.path + "\\"+ file.name
+                }*/
                 const dbFile = new File({
                     name: file.name,
                     type,
                     size: file.size,
-                    path: parent?.path,
+                    path: parent?._id,
+                    //path: filePath,
                     parent: parent?._id,
                     user: user._id
-                })
+                });
 
                 await dbFile.save()
                 await user.save()
@@ -91,6 +97,22 @@ class FileController {
             } catch (e) {
                 console.log(e)
                 res.status(500).json({message: "Download error"})
+            }
+        }
+        async deleteFile(req, res){
+            try{
+                const file =await File.findOne({_id: req.query.id, user: req.user.id})
+                if(!file){
+                    return res.status(400).json({message: 'Файл не найден'})
+                }
+                fileService.deleteFile(file)
+                await file.remove()
+                return res.json({message:'Файл удалён'})
+
+            }
+            catch(e){
+                console.log(e)
+                return res.status(400).json({message: 'Директория не пуста'})
             }
         }
 }
